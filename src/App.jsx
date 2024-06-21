e2da6747b0c0e5dc0cb4d137483f28be8a554c7f
Vicky Dhote <dhoteji034@gmail.com>
2024-06-21 20:58:43 +0530

    show img

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 const App = () => {
-  const [images,setImages]=useState()
+  const [images,setImages]=useState([])
   const getData=async()=>{
    const{data}= await axios.get("https://picsum.photos/v2/list")
    setImages(data)
@@ -12,18 +12,13 @@ const App = () => {
      <button onClick={getData} className="px-5 py-3  bg-green-500 rounded font-semibold">GetImage</button>
      
       <div>
-      {images < 0 ? (
-          images.map(function(elem, idx) {
+      {images.map(function(elem,idx) {
             return (
-              console.log(elem),
-              <div key={idx} className="p-2">
-                <img src={elem.download_url} alt={elem.author} className="w-32 h-32 m-2 rounded"/>
+              <div className="p-2 flex ">
+                <img src={elem.download_url}key={idx} className="w-32 h-32 m-2 rounded"/>
               </div>
             )
-          })
-        ) : (
-          <p>No images available</p>
-        )}
+          })}
       </div>
      </div>
   )
